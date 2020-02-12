af955c822255aa8e7d4a851ced4ea30fa5b30e21
Mike Sinnema <msinnema33@gmail.com>
2020-02-12 15:36:42 -0700

    worked on darkmode and localstorage

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,9 +1,24 @@
 import { useLocalStorage } from './useLocalStorage';
+import { useEffect } from 'react';
+
 
 const useDarkMode = () => {
-    const [someValue, setSomeValue] = useLocalStorage('');
-    
+    const [enabledState, setEnabledState] = useLocalStorage('dark-mode');
+    // const enabled = typeof enabledState !== 'undefined' ? enabledState : prefersDarkMode;
+    useEffect (
+        () => {
+            const className = 'dark-mode';
+            const element = window.document.body;
+            if (enabled) {
+                element.classList.add(className);
+            } else {
+                element.classList.remove(className);
+            }
+        }, [enabled]
+    );
 
 
-
+    return [enabled. setEnabledState];
 }
+
+export default useDarkMode;
